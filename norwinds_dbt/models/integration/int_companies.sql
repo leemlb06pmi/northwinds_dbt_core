de94efa746871e1b77dffdd4d44e5947262a0a3e
Miles Herr <miles@finexus.com>
2024-02-04 17:47:56 -0600

    added integration layer and 2 combined models

---
+++ norwinds_dbt/models/integration/int_companies.sql
@@ -0,0 +1,31 @@
+WITH hubspot_companies AS (
+    SELECT * FROM {{ ref('stg_hubspot_companies') }}
+),
+rds_companies AS (
+    SELECT * FROM {{ ref('stg_rds_companies') }}
+),
+merged_companies AS (
+    SELECT
+    h.company_id AS hubspot_company_id,
+    r.company_id as rds_company_id,
+    h.name
+    FROM hubspot_companies h
+    INNER JOIN rds_companies r
+        ON r.name = h.name
+),
+final AS (
+    SELECT
+    MAX(m.hubspot_company_id) as hubspot_company_id,
+    MAX(m.rds_company_id) as rds_company_id,
+    m.name,
+    MAX(r.address) as address, 
+    MAX(r.city) as city, 
+    MAX(r.postal_code) as postal_code, 
+    MAX(r.country) as country
+    FROM merged_companies m
+    LEFT JOIN rds_companies r
+        ON r.name = m.name
+    GROUP BY m.name
+)
+
+SELECT * FROM final
---
+++ norwinds_dbt/models/integration/int_contacts.sql
@@ -0,0 +1,33 @@
+WITH hubspot_contacts AS (
+    SELECT * FROM {{ ref('stg_hubspot_contacts') }}
+),
+rds_customers AS (
+    SELECT * FROM {{ ref('stg_rds_customers') }}
+),
+merged_customers AS (
+    SELECT
+    customer_id AS rds_contact_id, null as hubspot_contact_id, first_name, last_name, 
+    phone, company_id as rds_company_id, null as hubspot_company_id
+    FROM rds_customers
+    UNION ALL
+    SELECT
+    null as rds_contact_id, contact_id as hubspot_contact_id, first_name, last_name, 
+    phone, null as rds_company_id, company_id as hubspot_company_id
+    FROM
+    hubspot_contacts
+),
+final AS (
+    SELECT
+    MAX(rds_contact_id) as rds_contact_id, 
+    MAX(hubspot_contact_id) as hubspot_contact_id, 
+    first_name, 
+    last_name, 
+    MAX(phone) as phone, 
+    MAX(rds_company_id) as rds_company_id, 
+    MAX(hubspot_company_id) as hubspot_company_id
+    FROM
+    merged_customers
+    GROUP BY first_name, last_name
+)
+
+SELECT * FROM final
